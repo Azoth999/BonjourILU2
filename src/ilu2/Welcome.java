4339441a178b6b085fff9b888b5a9bdbad12ecbe
Azoth999 <yanispeyrat@gmail.com>
2023-04-20 17:34:00 +0200

    EX_2 (voir description)
    J'ai corrigé la présentation pour utiliser un StringBuilder au lieu de concaténer.
    Et void test() est bien devenu void EX_1().

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,7 +3,14 @@ package ilu2;
 public class Welcome {
 
 	public static String welcome(String input) {
-		return "Hello, " + input.substring(0, 1).toUpperCase() + input.substring(1);
+		StringBuilder reponse = new StringBuilder("Hello, ");
+		if (input == null || input.trim().isEmpty()) {
+			reponse.append("my friend");
+			return reponse.toString();
+		}
+		reponse.append(input.substring(0, 1).toUpperCase());
+		reponse.append(input.substring(1));
+		return reponse.toString();
 	}
 
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -7,8 +7,14 @@ import org.junit.jupiter.api.Test;
 class WelcomeTest {
 
 	@Test
-	void test() {
+	void ex_1() {
 		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
+	}
+
+	@Test
+	void ex_2() {
+		assertEquals("Hello, my friend", Welcome.welcome(null));
+		assertEquals("Hello, my friend", Welcome.welcome("  "));
 	}
 
 }
