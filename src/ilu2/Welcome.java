fbee8072e2d62996dbc1151b83c3ee40caa1a227
Azoth999 <yanispeyrat@gmail.com>
2023-05-01 12:51:14 +0200

    ITER_4
    Passage à ITER au lieu de EX pour suivre le sujet.
    Beaucoup de modifications pour rendre welcome moins lourd et faire un fonctionnement plus pratique.

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,32 +1,42 @@
 package ilu2;
 
 public class Welcome {
-
+	
+	
 	public String welcome(String input) {
 		StringBuilder reponse = new StringBuilder("Hello, ");
-		if (input == null || input.trim().isEmpty())
-			reponse = this.casNull(input);
-		return reponse.toString();
+		if (input == null || input.trim().isEmpty()) {
+			return casNull(reponse).toString();
+		}
+		if (input.split(",").length==2) {
+			return casDouble(input.split(","),reponse).toString();
+		}
 		if (input.equals(input.toUpperCase()))
-			return casUpperCase(input);
-		return casBase(input);
+			return casUpperCase(input,reponse);
+		return casBase(input,reponse);
 	}
 
-	private StringBuilder casNull(String input) {
+	private String casNull(StringBuilder reponse) {
 		reponse.append("my friend");
 		return reponse.toString();
 	}
 
-	private String casUpperCase(String input) {
+	private String casUpperCase(String input, StringBuilder reponse) {
 		reponse.append(input);
 		reponse.append("!");
-		return reponse.toString();
+		return reponse.toString().toUpperCase();
 	}
 
-	private String casBase(String input) {
+	private String casBase(String input, StringBuilder reponse) {
 		reponse.append(input.substring(0, 1).toUpperCase());
 		reponse.append(input.substring(1));
 		return reponse.toString();
 	}
-
+	
+	private String casDouble(String[] input, StringBuilder reponse) {
+		casBase(input[0], reponse);
+		reponse.append(", ");
+		casBase(input[1], reponse);
+		return reponse.toString();
+	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -2,29 +2,35 @@ package ilu2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
-
+	Welcome bonjour;
+	@BeforeEach
+	void initialisation() {
+		bonjour = new Welcome();
+	}
+	
 	@Test
 	void ex_1() {
-		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
+		assertEquals(bonjour.welcome("bob"), "Hello, Bob");
 	}
 
 	@Test
 	void ex_2() {
-		assertEquals("Hello, my friend", Welcome.welcome(null));
-		assertEquals("Hello, my friend", Welcome.welcome("  "));
+		assertEquals("Hello, my friend", bonjour.welcome(null));
+		assertEquals("Hello, my friend", bonjour.welcome("  "));
 	}
 
 	@Test
 	void ex_3() {
-		assertEquals("HELLO, JERRY!", Welcome.welcome("JERRY"));
+		assertEquals("HELLO, JERRY!", bonjour.welcome("JERRY"));
 	}
 
 	@Test
 	void ex_4() {
-		assertEquals("Hello, Amy, Bob", Welcome.welcome("amy", "bob"));
+		assertEquals("Hello, Amy, Bob", bonjour.welcome("amy,bob"));
 	}
 
 }
